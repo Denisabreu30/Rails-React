93f306fd032ad7c7e44b393b7c86f75e5a1f2326
denis <dennerkeskara@hotmail.com>
2022-05-16 13:56:15 -0300

    App CLone ifood

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,38 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only:[:show]
+
+  def index
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo')
+    filter_by_category if params[:category]
+    render json: @restaurants
+  end
+
+  def show
+    render json: @restaurant, product_categories: true
+  end
+
+  def search
+    @restaurants = Restaurant.search(
+      name_or_description_cont: params[:q]
+      ).result
+    @restaurants = @restaurants.near(params[:city]) if params[:city]
+    render json: @restaurants
+  end
+
+  private
+
+  def near
+    city = @restaurant.resaturant
+    render json: Restaurant.near([city.latitude, city.longitude], 5, units: :km)
+  end
+
+  def set_restaurant
+     @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
+  end
+end
---
+++ app/serializers/product_category_serializer.rb
@@ -0,0 +1,8 @@
+class ProductCategorySerializer < ActiveModel::Serializer
+
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title
+ 
+  has_many :products
+end
